ad1ddc8c55b2469e5818c9e02cafe0aecca2cbbb
Garv Banerji <61663667+BarrelKnight723@users.noreply.github.com>
2021-11-13 00:55:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Database = require("@replit/database")
 const db = new Database()
 const client = new Discord.Client()
 
+
+
 client.on('ready', () =>
 {
   console.log('Logged in!')
@@ -12,27 +14,17 @@ client.on('ready', () =>
 
 client.on('message', msg =>
 {
+  var interval = 
+  setInterval(function()
+  {
+    msg.channel.send("eat your vegetables!")
+  })
+    
   if (msg.content === '!spam')
   {
     msg.channel.send('Let the games begin!')
-    msg.channel.send('eat your vegetables!')
+    interval
   }
-
-  if (msg.content === 'eat your vegetables!')
-  {
-    msg.channel.send('eat your vegetables!')
-  }
-
-  else
-  {
-    msg.channel.send('eat your vegetables!')
-  }
-
-  if (msg.content === '!stop')
-  {
-    stop
-  }
-})
 
 keepAlive()
 client.login(process.env.TOKEN)
